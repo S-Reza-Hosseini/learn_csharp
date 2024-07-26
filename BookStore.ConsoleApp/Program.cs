9754a2d9ae00aa807645ac52d8873e961ea7035c
reza hosseini <hreza8254@gmail.com>
2024-07-26 21:34:29 +0330

    Main function argumans

---
+++ BookStore.ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//Console.WriteLine("Hello, World!");
+public class Program {
+
+    public static void Main(string[]args){
+
+        foreach(string arg in args)
+        {
+            Console.WriteLine(arg);
+        }
+
+    }
+}
+
