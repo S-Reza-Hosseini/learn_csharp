481dc651fe1abb8831c58fe06dbcfc85a3cdb7c1
reza hosseini <hreza8254@gmail.com>
2024-07-27 00:42:59 +0330

    tamrin tavabe add shod

--- BookStore.ConsoleApp/Program.cs
+++ BookStore.ConsoleApp/Program.cs
@@ -1,16 +1,85 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-//Console.WriteLine("Hello, World!");
-public class Program {
+// tamri 1
+// function with input and output
+using System.Collections;
+using System.Globalization;
+using System.Runtime.InteropServices;
 
-    public static void Main(string[]args){
 
-        foreach(string arg in args)
-        {
-            Console.WriteLine(arg);
-        }
 
+
+static int Sumation2Adad(int num1, int num2)
+{
+    return num1+num2;
+}
+// function without input and output
+static void printMeaningLessString()
+{
+    Console.WriteLine("i have realy doesn't have action but i am here !");
+}
+// function with input and without output
+static void FindMax(int []numbers)
+{
+    int max = 0;
+    for(int i = 0; i < numbers.Length; i++){
+
+        max = numbers[0] ;
+
+        if (numbers[i]> max)
+            max = numbers[i];
     }
+
+    Console.WriteLine($"max in numbers array is {max} !!!");
+}
+
+// function without input and with output
+static int GetRandomNumber()
+{
+    Random random = new Random();
+
+    return random.Next(1, 100);
+}
+
+
+
+
+// tamrin 2
+// part 1
+//Console.WriteLine(DescendingReturner(GetNumber("Enter number for returnal Descending :")));
+
+printTogether("ali", "reza", "mmad" , "hashem");
+
+static string DescendingReturner(int number )
+{
+    
+    if (number == 0 || number == 1)
+
+        return "1";
+
+    return $"{number} , {DescendingReturner(number- 1)}";
+}
+
+static int GetNumber(string msg) 
+{
+    bool canParseNumber = false;
+    var number = 0;
+    while(!canParseNumber){
+    Console.WriteLine(msg);
+    canParseNumber = int.TryParse(Console.ReadLine(), out number);
+    }
+
+    return number;
+}
+
+
+//tamrin 2
+// part 2
+
+
+static void printTogether(params string[] name )
+{
+    Console.WriteLine($"{string.Join(',' , name)}");
 }
 
